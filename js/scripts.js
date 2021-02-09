e3dc291e8f04cdee1f71adf69e2a4a4338b4195e
Rodrigo Luiz <dolbrebr@gmail.com>
2021-02-09 13:22:27 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const Modal = {
+  open() {
+    // abrir modal
+    // adicionar a class active ao modal
+    document
+      .querySelector('.modal-overlay')
+      .classList.add('active')
+
+  },
+  close() {
+    // fechar modal
+    // remover a class active ao modal
+    document
+      .querySelector('.modal-overlay')
+      .classList.remove('active')
+  }
+}
